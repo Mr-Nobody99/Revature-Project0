edcab253e02fb9193571e02dd2afe5bbe769a70a
Mr-Nobody99 <Kutzy99@gmail.com>
2019-08-24 06:55:06 -0400

    somplified screen wraping function

--- src/Components/GameObject.js
+++ src/Components/GameObject.js
@@ -10,30 +10,20 @@ class GameObject extends Object3D{
     }
 
     screenLoop(camera){//Function used to loop object around screen
-
-        //Create a new vector from current location. This will be used to determine which edge the object has moved into.
-        let screenPos = new THREE.Vector3(this.position.x, this.position.y, this.position.z);
+        let screenPos = this.position.clone();
         screenPos.project(camera);//Project the vector to screen space(ie. cameras view);
         screenPos.x = ( screenPos.x + 1 ) * window.innerWidth / 2 ;//Map the projected vector
         screenPos.y = ( screenPos.y + 1 ) * window.innerHeight / 2 ;// using basic conversion formula.
         screenPos.z = 0;
 
-        //Creating a new target world space loaction using the screen space position
-        //This will be used to as the target location to warp the obect to.
-        let worldPos = new THREE.Vector3((screenPos.x / window.innerWidth) * 2 - 1,
-                                        (screenPos.y / window.innerHeight) * 2 - 1,
-                                        0.5);
-        worldPos.unproject(camera);//project back into world space;
+        let newPos = this.position.clone();
+        newPos.multiplyScalar(-1);
 
         if(screenPos.y > 0 && screenPos.y < window.innerHeight){
-            let x = (100 * worldPos.x);
-            if(screenPos.x <= 0) { this.position.setX(-x + 5); }
-            else if(screenPos.x >= 0) { this.position.setX(-x - 5); }
+            this.position.setX(newPos.x);
         }
         if(screenPos.x > 0 && screenPos.x < window.innerWidth){
-            let y = (100 * worldPos.y);
-            if(screenPos.y <= 0) { this.position.setY(-y + 3); }
-            else if(screenPos.y >= 0) { this.position.setY(-y - 3); }
+            this.position.setY(newPos.y);
         }
     }
 
